c1e91db5d972434ea5c0bc6aee3bbec05c958342
ArcherAJ <155043694+ArcherAJ@users.noreply.github.com>
2025-05-05 19:38:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showMessage(type) {
-    // Hide all messages
-    document.getElementById('message-sorry').style.display = 'none';
-    document.getElementById('message-stupid').style.display = 'none';
-    document.getElementById('message-forgive').style.display = 'none';
-    
-    // Show the clicked message
-    document.getElementById(`message-${type}`).style.display = 'block';
-  )
+  // Hide all messages
+  document.getElementById('message-sorry').style.display = 'none';
+  document.getElementById('message-stupid').style.display = 'none';
+  document.getElementById('message-forgive').style.display = 'none';
+  
+  // Show the clicked message
+  document.getElementById(`message-${type}`).style.display = 'block';
+}
